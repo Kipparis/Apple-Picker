db6ee46f7f80c781515294c20a14e106027d0e15
Un <keepintine@gmail.com>
2018-06-24 01:55:52 +0300

    Add main mechanic

--- Apple Picker Prototype/Assets/AppleTree.cs
+++ Apple Picker Prototype/Assets/AppleTree.cs
@@ -4,13 +4,51 @@ using UnityEngine;
 
 public class AppleTree : MonoBehaviour {
 
+    // Яблочко
+    public GameObject applePrefab;
+
+    // Скорость движения дерева
+    public float speed = 10f;
+
+    // Расстояние на котором дерево поворачивает
+    public float leftAndRitghtEdge = 18f;
+
+    // Шанс с которым дерево поменяет направление
+    public float chanceToChangeDirections = 0.1f;
+
+    // Как часто падают яблоки
+    public float secondsBetweenAppleDrops = 1f;
+
 	// Use this for initialization
 	void Start () {
-		
+        // Выбрасывание яблок каждую секунду	
+        InvokeRepeating("DropApple", 2f, secondsBetweenAppleDrops);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+    void DropApple() {
+        GameObject apple = Instantiate(applePrefab) as GameObject;
+        apple.transform.position = this.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        // Движение
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+        // Смена движения
+        if (pos.x < -leftAndRitghtEdge) {
+            speed = Mathf.Abs(speed);
+        } else
+        if (pos.x > leftAndRitghtEdge) {
+            speed = -Mathf.Abs(speed);
+        }   
 	}
+
+    void FixedUpdate() {
+        // Смена направления рандомно
+        if(Random.value < chanceToChangeDirections) {
+            speed *= -1;
+        }
+    }
 }
--- Apple Picker Prototype/Assets/Basket.cs
+++ Apple Picker Prototype/Assets/Basket.cs
@@ -4,13 +4,32 @@ using UnityEngine;
 
 public class Basket : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        Vector3 mousePos2D = Input.mousePosition;
+
+        // Координата z камеры говорит как далеко протолкнуть мыш
+        mousePos2D.z = -Camera.main.transform.position.z;
+
+        // Конвертируем
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+
+        // Изменяем х координату корзинки
+        Vector3 pos = this.transform.position;
+        pos.x = mousePos3D.x;
+        this.transform.position = pos;
+
+    }
+
+    void OnCollisionEnter(Collision coll) {
+        GameObject collidedWith = coll.gameObject;
+        if (collidedWith.tag == "Apple") {
+            Destroy(collidedWith);
+        }
+    }
 }
